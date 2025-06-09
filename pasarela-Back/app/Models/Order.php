8b590e951b05f2f4363016dd204a5b21811e555f
PieroMS <piero.mendieta6@gmail.com>
2025-06-09 17:27:35 -0500

    paypal backend

--- pasarela-Back/app/Models/Order.php
+++ pasarela-Back/app/Models/Order.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Oder extends Model
+class Order extends Model
 {
     use HasFactory;
+    
     protected $fillable = [
         'order_number',
         'user_id',
@@ -15,6 +16,6 @@ class Oder extends Model
         'total',
         'payment_gateway',
         'external_order_id',
-        'status'
+        'status',
     ];
 }
